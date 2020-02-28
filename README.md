b01270b2acdb24831d6ea3c69b1069e3fdb14083
Rob Porter <rob@weeverapps.com>
2020-02-28 15:13:52 -0500

    Fixes description

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
-# Task List Zero GH Action
+# Task List Zero GitHub Action
 
 This action checks to see if a PR's task lists are all checked, and passes status checks if so.
+
+# How to add
+
+```
+    - name: Task List Zero
+      uses: WeeverApps/gha-task-list-zero@v1.0
+      env:
+        GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+```
--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-//const core = require('@actions/core');
-//const github = require('@actions/github');
-
-
 module.exports = robot => {
   robot.on(
     [
@@ -21,36 +17,18 @@ module.exports = robot => {
           .html_url}): ${status}`
       );
 
-      robot.log(`Body: ${body}`);
-
       robot.log(`isUnchecked: ${isUnChecked}`);
 
       context.github.repos.createStatus(
         context.repo({
           sha: context.payload.pull_request.head.sha,
           state: status,
-        //  target_url: "https://github.com/settings/apps/task-list-zero",
-          description: status
-            ? "task list not completed yet"
-            : "ready for the next steps",
+          description: isUnChecked
+            ? "The task list is not completed yet"
+            : "The task list is completed",
           context: "Task List Zero"
         })
       );
     }
   );
 };
-
-/*
-try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
-    const time = new Date().toTimeString();
-    core.setOutput('time', time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2);
-    console.log(`The event payload: ${payload}`);
-} catch (error) {
-    core.setFailed(error.message);
-}
-*/
